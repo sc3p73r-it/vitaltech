79ac52a14b36916c1c62f01d65161eaefe1bdbf3
sc3p73r-it <sc3p73r@outlook.com>
2023-11-10 15:57:21 +0630

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/service', function () {
+    return view('service');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/price', function () {
+    return view('price');
+});
+
+Route::get('/feature', function () {
+    return view('feature');
+});
+
+Route::get('/team', function () {
+    return view('team');
+});
+
+Route::get('/testimonial', function () {
+    return view('testimonial');
+});
+
+Route::get('/quote', function () {
+    return view('quote');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
